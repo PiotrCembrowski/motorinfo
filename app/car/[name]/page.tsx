06b705932a14a2c407ac8b4de35bc998781a3d18
Piotr Cembrowski <losttape@gmail.com>
2025-01-07 03:41:17 +0100

    adding heading url name to page

--- app/car/[name]/page.tsx
+++ app/car/[name]/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { headers } from "next/headers";
 
-const page = () => {
-  return <div>pate</div>;
+const page = async () => {
+  const headerList = await headers();
+  const pathname = headerList.get("x-url");
+  const url = pathname?.substring(pathname.lastIndexOf("/") + 1);
+  const name = url?.replace(/%20/g, " ");
+
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
 };
 
 export default page;
