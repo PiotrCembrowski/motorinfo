660dcc76a703ca1433caf3457a299e95e90bcf62
Piotr Cembrowski <losttape@gmail.com>
2025-01-07 04:46:21 +0100

    styles

--- app/car/[name]/page.tsx
+++ app/car/[name]/page.tsx
@@ -13,18 +13,37 @@ const page = async () => {
       <h1 className="capitalize">{name}</h1>
       <h2>Gen. 1</h2>
       <div className="grid grid-cols-2">
-        <div className="bg-white py-4 px-8">
+        <div className="bg-white py-4 px-8 shadow-sm">
+          <table>
+            <tbody>
+              <tr className="">
+                <td className="w-1/2">Engine type</td>
+                <td>Turbo charged with VTEC</td>
+              </tr>
+              <tr>
+                <td>Displacment</td>
+                <td>2000 cc3</td>
+              </tr>
+              <tr>
+                <td>Power</td>
+                <td>329 hp</td>
+              </tr>
+              <tr>
+                <td>Cylinders</td>
+                <td>4</td>
+              </tr>
+              <tr>
+                <td>Turbo</td>
+                <td>Yes</td>
+              </tr>
+              <tr>
+                <td>Torque</td>
+                <td>420 Nm</td>
+              </tr>
+            </tbody>
+          </table>
           <div>
-            <h3>Engine: </h3>
-            <p>Turbo charged with VTEC</p>
-            <p>2000 cc3</p>
-            <p>Cylinder: 4 cylinders</p>
-            <p>Turbo</p>
-            <p>329 KM</p>
-            <p>torque: 420 Nm</p>
-          </div>
-          <div>
-            <h3>Acceleration and stuff:</h3>
+            <h4>Acceleration and stuff:</h4>
             <p>0-100 km/h - 5,4 s</p>
             <p>
               v-max: 2 minuty 23 sekundy: Type R to najszybszy samochód z
@@ -33,12 +52,12 @@ const page = async () => {
             </p>
           </div>
           <div>
-            <h3>Transmission:</h3>
+            <h4>Transmission:</h4>
             <p>6-gears</p>
             <p>manual</p>
           </div>
           <div>
-            <h3>Measurements: </h3>
+            <h4>Measurements: </h4>
             <p>wysokość</p>
             <p>szerokość</p>
             <p>długość</p>
