ec1153de74c2e685d990e24e454efc7330dc7b41
Piotr Cembrowski <losttape@gmail.com>
2025-01-07 03:56:20 +0100

    adding gen and engine divs"

--- app/car/[name]/page.tsx
+++ app/car/[name]/page.tsx
@@ -9,7 +9,19 @@ const page = async () => {
 
   return (
     <div className="w-[1440px] m-auto mt-11">
-      <h1>{name}</h1>
+      <h1 className="capitalize">{name}</h1>
+      <h2>Gen. 1</h2>
+      <div>
+        <div>
+          <h3>Engine: </h3>
+          <p>Turbo charged with VTEC</p>
+          <p>2000 cc3</p>
+          <p>Cylinder: 4 cylinders</p>
+          <p>Turbo</p>
+          <p>329 KM</p>
+          <p>torque: 420 Nm</p>
+        </div>
+      </div>
     </div>
   );
 };
