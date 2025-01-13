762e1ac08affad4119c37d4b5c63a468956a4387
Piotr Cembrowski <losttape@gmail.com>
2025-01-13 15:48:55 +0100

    reading data from file

--- app/[car]/page.tsx
+++ app/[car]/page.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { headers } from "next/headers";
+import { promises as fs } from "fs";
 
 const page = async () => {
   const headerList = await headers();
   const pathname = headerList.get("x-url");
   const url = pathname?.substring(pathname.lastIndexOf("/") + 1);
   const name = url?.replace(/%20/g, " ");
-  return <div>{name}</div>;
+
+  const car = await fs.readFile(process.cwd() + "/data/cars.json", "utf-8");
+  const data = JSON.parse(car);
+  console.log(data);
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{data.name}</p>
+    </div>
+  );
 };
 
 export default page;
