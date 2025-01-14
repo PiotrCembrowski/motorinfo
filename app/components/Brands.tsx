6e992c131aa81aa0d86d71e658eca4f22e6793e9
Piotr Cembrowski <losttape@gmail.com>
2025-01-14 15:24:26 +0100

    adding brands

---
+++ app/components/Brands.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Brands = () => {
+  return (
+    <div className="w-[1200px] mx-auto my-12">
+      <h1>Brands</h1>
+    </div>
+  );
+};
+
+export default Brands;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Brands from "./components/Brands";
 import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
+      <Brands />
     </div>
   );
 }
