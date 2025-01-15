55dac1915ce9f9ba972d2f00ede1fa7dc469c786
Piotr Cembrowski <losttape@gmail.com>
2025-01-15 13:10:52 +0100

    brand type added

--- app/components/brands/Brands.tsx
+++ app/components/brands/Brands.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import BrandItem from "./BrandItem";
 import { promises as fs } from "fs";
 
+type Brand = {
+  name: string;
+  url: string;
+};
+
 const Brands = async () => {
-  const brand = await fs.readFile(process.cwd() + "/data/brands.json", "utf-8");
+  const brand = await fs.readFile(process.cwd() + "data/brands.json", "utf-8");
   const data = JSON.parse(brand);
-  console.log(data);
 
   return (
     <div className="w-[1200px] mx-auto my-12">
       <h1>Brands</h1>
       <div className="grid grid-cols-10">
-        <BrandItem />
+        {data.map((el: Brand) => {
+          <BrandItem brand={el} />;
+        })}
       </div>
     </div>
   );
