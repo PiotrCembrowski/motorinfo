43956201d300ad50ff558b95a9323808136f6023
Piotr Cembrowski <losttape@gmail.com>
2025-01-15 14:10:10 +0100

    working car items

--- app/[car]/page.tsx
+++ app/[car]/page.tsx
@@ -9,13 +9,20 @@ const page = async () => {
   const url = pathname?.substring(pathname.lastIndexOf("/") + 1);
   const name = url?.replace(/%20/g, " ");
 
-  const car = await fs.readFile(process.cwd() + "/data/cars.json", "utf-8");
+  const car = await fs.readFile(process.cwd() + "/data/Honda.json", "utf-8");
   const data = JSON.parse(car);
+  console.log(data);
 
   return (
     <div>
       <h1>{name}</h1>
-      <Link href={`honda/${data.name}`}>{data.name}</Link>
+      {data.map((car, index) => {
+        return (
+          <Link href={`honda/${car.model}`} key={index}>
+            {car.model}
+          </Link>
+        );
+      })}
     </div>
   );
 };
