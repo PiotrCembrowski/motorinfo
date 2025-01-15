83a707a851824cff59bb8b474f6c9965bc365de9
Piotr Cembrowski <losttape@gmail.com>
2025-01-15 12:52:52 +0100

    adding honda logo data

--- app/components/brands/BrandItem.tsx
+++ app/components/brands/BrandItem.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import img from "@/public/images/honda-logo.png";
 
 const BrandItem = () => {
   return (
     <Link href="Honda">
       <div className="text-center">
-        <img src="" alt="Honda" />
+        <Image src={img} alt="Honda" />
         <h3>Honda</h3>
       </div>
     </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Brands from "./components/brands/Brands";
+import img from "@/public/images/honda-logo.png";
 
 export default function Home() {
   return (
