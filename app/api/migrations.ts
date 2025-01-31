eba8c5a54fc2a645de3d168c4476976ca99be8ac
Piotr Cembrowski <losttape@gmail.com>
2025-01-31 13:02:13 +0100

    brands migraion

---
+++ app/api/migrations.ts
@@ -0,0 +1,21 @@
+import { db } from "./database";
+
+export const migrate = () => {
+  db.serialize(() => {
+    db.run(
+      `
+            CREATE TABLE IF NOT EXISTS brands (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                image_url TEXT NOT NULL
+            )
+        `,
+      (err: Error) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log("Brands table created successfuly.");
+      }
+    );
+  });
+};
