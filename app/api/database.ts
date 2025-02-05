760076ed4d80f125bae2b7ceaeafaaf2e579b2f1
Piotr Cembrowski <losttape@gmail.com>
2025-02-05 13:05:36 +0100

    rearanging data

--- app/api/database.ts
+++ app/api/database.ts
@@ -12,3 +12,27 @@ export const db = new sqlite3.Database(
     console.log("Connected to the profile database.");
   }
 );
+
+export const apiGet = async (query: string) => {
+  return await new Promise((resolve, reject) => {
+    db.all(query, (err: Error, row: unknown[]) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      return resolve(row);
+    });
+  });
+};
+
+export const apiPost = async (query: string, values: string[]) => {
+  return await new Promise((resolve, reject) => {
+    db.run(query, values, function (err) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      resolve(null);
+    });
+  });
+};
--- app/api/migrations.ts
+++ app/api/migrations.ts
@@ -19,27 +19,3 @@ export const migrate = () => {
     );
   });
 };
-
-export const apiGet = async (query: string) => {
-  return await new Promise((resolve, reject) => {
-    db.all(query, (err: Error, row: unknown[]) => {
-      if (err) {
-        console.log(err);
-        return reject(err);
-      }
-      return resolve(row);
-    });
-  });
-};
-
-export const apiPost = async (query: string, values: string[]) => {
-  return await new Promise((resolve, reject) => {
-    db.run(query, values, function (err) {
-      if (err) {
-        console.log(err);
-        reject(err);
-      }
-      resolve(null);
-    });
-  });
-};
