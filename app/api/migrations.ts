9829871982122ccafbe5ef16ae9ebdad5ace10a3
Piotr Cembrowski <losttape@gmail.com>
2025-02-13 14:28:50 +0100

    table added to migrate

--- app/api/migrations.ts
+++ app/api/migrations.ts
@@ -18,4 +18,37 @@ export const migrate = () => {
       }
     );
   });
+
+  db.run(
+    `
+    CREATE TABLE IF NOT EXISTS cars (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        model TEXT NOT NULL,
+        segment TEXT NOT NULL,
+        door_options TEXT NOT NULL,
+        silniki_benzyna TEXT,
+        silniki_diesel TEXT,
+        silniki_hybrid TEXT,
+        silniki_electric TEXT,
+        silniki_moc TEXT NOT NULL,
+        silniki_moment_obrotowy TEXT NOT NULL,
+        charakterystyka TEXT NOT NULL,
+        mileage TEXT NOT NULL,
+        acceleration TEXT NOT NULL,
+        v_max TEXT NOT NULL,
+        gears INTEGER NOT NULL,
+        transmission_type TEXT NOT NULL,
+        height TEXT NOT NULL,
+        weight TEXT NOT NULL,
+        length TEXT NOT NULL
+    );
+    `,
+    (err: Error) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log("Cars table created successfully.");
+      }
+    }
+  );
 };
