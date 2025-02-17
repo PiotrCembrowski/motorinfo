ccb53bdbb01eae7eddb4941124ec5507d158ad13
Piotr Cembrowski <losttape@gmail.com>
2025-02-17 14:31:23 +0100

    fixing values insertion

--- app/api/brands/cars/route.ts
+++ app/api/brands/cars/route.ts
@@ -26,26 +26,33 @@ export async function POST(req: Request, res: Response) {
 
   const query = `
   INSERT INTO cars(
-    company,
-    model,
-    segment,
-    door_options,
-    silniki_benzyna,
-    silniki_diesel,
-    silniki_hybrid,
-    silniki_electric,
-    silniki_moc,
-    silniki_moment_obrotowy,
-    charakterystyka,
-    mileage,
-    acceleration,
-    v_max,
-    gears,
-    transmission_type,
-    height,
-    weight,
-    length)
-  VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        company TEXT NOT NULL,
+        model TEXT NOT NULL,
+        segment TEXT NOT NULL,
+        door_options TEXT NOT NULL,
+        silniki_benzyna TEXT,
+        silniki_diesel TEXT,
+        silniki_hybrid TEXT,
+        silniki_electric TEXT,
+        benzyna_moc TEXT,
+        diesel_moc TEXT,
+        hybrid_moc TEXT,
+        electric_moc TEXT,
+        benzyna_moment_obrotowy TEXT NOT NULL,
+        diesel_moment_obrotowy TEXT NOT NULL,
+        hybrid_moment_obrotowy TEXT NOT NULL,
+        electric_moment_obrotowy TEXT NOT NULL,
+        charakterystyka TEXT NOT NULL,
+        mileage TEXT NOT NULL,
+        acceleration TEXT NOT NULL,
+        v_max TEXT NOT NULL,
+        gears INTEGER NOT NULL,
+        transmission_type TEXT NOT NULL,
+        height TEXT NOT NULL,
+        weight TEXT NOT NULL,
+        length TEXT NOT NULL)
+  VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
 `;
 
   const values = [
