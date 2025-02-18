f8219a8ab8cdbf49df2f70c0a20f4afc422533a7
Piotr Cembrowski <losttape@gmail.com>
2025-02-18 14:40:48 +0100

    creating fetch func reusable

---
+++ app/utils/fetch.ts
@@ -0,0 +1,10 @@
+export async function fetchData<T>(utl: string): Promise<T> {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data. Status: ${response.status}`);
+  }
+
+  const data: T = await response.json();
+  return data;
+}
