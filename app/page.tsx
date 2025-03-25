0c0aca62f32fea5b2a9043e62656d06bc9a5e521
Piotr Cembrowski <losttape@gmail.com>
2025-03-25 14:17:52 +0100

    adding hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Brands from "./components/brands/Brands";
+import Hero from "./components/hero/Hero";
 
 export default function Home() {
   return (
     <div className="">
+      <Hero />
       <Brands />
     </div>
   );
